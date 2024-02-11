8a7c67c26e5828f1667b681146eca02cfa032ef2
FadelYunus07 <fadelyunus07@gmail.com>
2024-02-11 14:44:52 +0700

    perbaiki map

--- src/Component/FooterComponent.jsx
+++ src/Component/FooterComponent.jsx
@@ -39,10 +39,10 @@ function FooterComponent() {
         <div className="lg:flex items-center md:hidden hidden">
           <h1 className="text-xl font-bold mx-auto ps-[20px]">Find Us Here: </h1>
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d987.0079378739505!2d114.28270236949284!3d-8.299638899483444!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd15128f746fb01%3A0x19afdcd97decb907!2sJl.%20Perumahan%20Pengatigan%20Indah%2C%20Guri%2C%20Pengatigan%2C%20Kec.%20Rogojampi%2C%20Kabupaten%20Banyuwangi%2C%20Jawa%20Timur%2068462!5e0!3m2!1sen!2sid!4v1705501821766!5m2!1sen!2sid"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15792.124788531608!2d114.2833878135659!3d-8.299694049488387!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd151004b80e0a5%3A0x16562af1b4a91a11!2sEva%20Aqiqah%20%26%20Catering!5e0!3m2!1sen!2sid!4v1707637255525!5m2!1sen!2sid"
             width="400"
             height="300"
-            className="mx-auto border-[2px] border-white rounded-lg"
+            className="border-[2px] border-white rounded-md me-5"
             allowfullscreen=""
             loading="lazy"
             referrerpolicy="no-referrer-when-downgrade"
